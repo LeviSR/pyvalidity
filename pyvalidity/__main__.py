b3273034dae365ffe787b16f231de61b227c37cc
Levi Ryffel <levi.ryffel@gmail.com>
2020-06-22 10:50:43 +0200

    added streamlined execution for __main__.py

--- pyvalidity/__main__.py
+++ pyvalidity/__main__.py
@@ -1,2 +1,13 @@
+from Parser import Parser
 
+print("Please enter an l-group (in)equation using the characters '^', 'v' for ",
+      "meets, joins, respectively, prefixed '-' for inverses, and one-character ",
+      "variables and capital letters for their inverse.")
+print("E.g.: \nX v Y = -(x ^ y)\n(x ^ e)(x ^ e) <= Y(x ^ e)y")
+print("\nYour Input: ")
 
+while True:
+    string = input()
+    parsed = Parser(string).parse()
+    print("The formula ", str(parsed), " you entered is " + "valid." if parsed.is_valid() else "invalid.")
+    print("\nYour Input: ")
