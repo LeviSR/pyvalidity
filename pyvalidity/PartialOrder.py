df460927d688d95eab97602e92316346280ec1ba
Levi Ryffel <levi.ryffel@gmail.com>
2020-06-22 10:28:20 +0200

    made a whacky part a bit more consistent, but this still needs work.

--- pyvalidity/PartialOrder.py
+++ pyvalidity/PartialOrder.py
@@ -15,7 +15,7 @@ class PartialOrder:
 
         if complement is None:
             self.complement = truncated_group.elements.copy()
-            self.complement -= self.positives
+            self.complement -= self.positives | {x.inv() for x in self.positives}
             if GroupTerm([]) in self.complement:
                 self.complement.remove(GroupTerm([]))
         else:
@@ -26,28 +26,14 @@ class PartialOrder:
         if GroupTerm([]) in self.positives:
             return False
         assert GroupTerm([]) not in self.complement
-        if all(t in self.positives or t.inv() in self.positives
-               for t in self.complement):
+        if not self.complement:
             return True
         # now there exists t in ambient_set such that neither
         # t nor t^{-1} are in positives.
         assert self.complement != set()
 
-        # should be equivalent to:
-        # candidates = [t for t in self.complement if t not in self.positives and t.inv() not in self.positives]
-        # t = min(candidates, key=len)for t in self.complement:
+        t = min(self.complement, key=len)
 
-        started = False
-        for t in self.complement:
-            if not started:
-                best_t = t
-                started = True
-            if len(t) <= 2:
-                best_t = t
-                break
-            if len(t) <= len(best_t):
-                best_t = t
-        t = best_t
         # t is now a candidate to extend with.
         assert t not in self.positives and t.inv() not in self.positives
         for s in [t, t.inv()]:
@@ -56,7 +42,7 @@ class PartialOrder:
                                                 True)
             new_set.add(s)
             new_complement = self.complement.copy()
-            new_complement.remove(s)
+            new_complement -= new_set.elements | {x.inv() for x in new_set.elements}
             child = PartialOrder(new_set, self.truncated_group, new_complement)
             if child.extends_to_total_order():
                 return True
--- test/test_pyvalidity.py
+++ test/test_pyvalidity.py
@@ -6,25 +6,25 @@ from Parser import Parser
 class TestSantschisEquations(TestCase):
     def setUp(self) -> None:
         self.valid_strings = [
-            # "x ^ (y v z) = (x ^ y) v (x ^ z) ",
-            # "x(y v z)w = (xyw) v (xzw)",
-            # "x(y ^ z)w = (xyw) ^ (xzw) ",
-            # "X ^ Y = -(x v y)",
-            # "X v Y = -(x ^ y)",
-            # "e <= x v X",
-            # "(xy) ^ e <= x v y",
-            # "e <= (xx) v (yy) v (XY)",
-            # "((Xy) ^ e) v ((Yx) ^ e) = e",
-            # "((xY) ^ e) v ((yX) ^ e) = e"
+            "x ^ (y v z) = (x ^ y) v (x ^ z) ",
+            "x(y v z)w = (xyw) v (xzw)",
+            "x(y ^ z)w = (xyw) ^ (xzw) ",
+            "X ^ Y = -(x v y)",
+            "X v Y = -(x ^ y)",
+            "e <= x v X",
+            "(xy) ^ e <= x v y",
+            "e <= (xx) v (yy) v (XY)",
+            "((Xy) ^ e) v ((Yx) ^ e) = e",
+            "((xY) ^ e) v ((yX) ^ e) = e"
         ]
 
         self.invalid_strings = [
-            # "e <= (xx) v (xy) v (yX) ",
-            #"(x ^ e)(x ^ e) <= Y(x ^ e) y",         # this one is hard apparently, but doable.
+             "e <= (xx) v (xy) v (yX) ",
+            "(x ^ e)(x ^ e) <= Y(x ^ e) y",         # this one is hard apparently, but doable.
                                                     # expect it to take 1-5 seconds. it is not
                                                     # consistent how long it takes.
-            # "e <= x v (yXY)",
-             "(xyz) ^ (rst) <= (xsz) v (ryt)"      # very hard.
+            "e <= x v (yXY)",
+            # "(xyz) ^ (rst) <= (xsz) v (ryt)"      # very hard.
         ]
 
     def test_valids(self):
